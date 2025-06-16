a43174004ed6e2b26c345f4481bfbe900fc509a4
Bjoern Ammon <bjoern.ammon1991@gmail.com>
2025-06-16 23:17:00 +0200

    adjust type

--- googleCloud/modules/firebase/firestore/main.tf
+++ googleCloud/modules/firebase/firestore/main.tf
@@ -1,20 +1,22 @@
 resource "google_firebase_project" "default" {
-  project = var.project_id
+  provider = google-beta
+  project  = var.project_id
 }
 
 resource "google_firestore_database" "default" {
+  provider    = google-beta
   name        = "(default)"
   project     = var.project_id
   location_id = "europe-west3"
   type        = "FIRESTORE_NATIVE"
 }
 
-// TTL-Rule: Delete device documents automatically after expireAt field
 resource "google_firestore_field" "device_expireAt" {
-  project    = var.project_id
-  database   = google_firestore_database.default.name
-  collection = "device"
-  field      = "expireAt"
+  provider    = google-beta
+  project     = var.project_id
+  database    = google_firestore_database.default.name
+  collection  = "device"
+  field       = "expireAt"
   ttl_config {
     state = "ENABLED"
   }
